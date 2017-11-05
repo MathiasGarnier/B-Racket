d3a006a19473db3f4ca68f42f52a1473bd3bc6ec
Mathias Garnier <mathias.garnier69000@gmail.com>
2017-11-05 16:30:49 +0100

    Add files via upload

---
+++ Basics/First Cond about the fucking butterfly.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (switch_alternative_also_called_a_cond str)
+  (cond
+    [(string-contains? str "butterfly")
+     "Fuck, the fucking butterfly. We can't kill it now, we're too weak !"]
+    [( >= (string-length str) 42)
+     "Shut up, let me think, I try to found a solution to kill the fucking butterfly"]
+    [else "Hmmm, I don't understand"]))
+
+(define butterfly "Ohhhh, I would kill the butterfly")
+(define shut_up "Blablablablablablablablablablablablablablablablablablablablablablabla")
+(define other "2 + 3 = -1")
+
+(switch_alternative_also_called_a_cond butterfly)
+(switch_alternative_also_called_a_cond shut_up)
+(switch_alternative_also_called_a_cond other)
